2acb87b1cdde6fc08f1c420f09490fbdfa7ca830
Olaf Hoogenboom <olaf_hoogenboom@hotmail.com>
2019-09-10 09:46:21 +0200

    hello world

--- src/main/java/olaf/novi/gittest/Main.java
+++ src/main/java/olaf/novi/gittest/Main.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package olaf.novi.gittest;
 
-/**
- *
- * @author elena pc
- */
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
     
 }
